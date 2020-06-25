5296cd6d7a5d3400d9e5ec96d72e509209077f6a
AlfredMescheryakov <ameshcheryakov@plazius.ru>
2020-06-25 17:49:04 +0300

    Deleted script revert VMs

--- prometheus_stack(revert_and_start_vms_in_kvm).sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for node_name in node_exporter_1 node_exporter_2;
-  do
-	virsh snapshot-revert --domain ${node_name} --snapshotname $(virsh snapshot-list --domain ${node_name} --name) --running;
-	echo "Revert and started ${node_name}";
-  done
